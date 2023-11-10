4755ef67989607b2495d2982dcdce5963a40a072
DewGoody <dewzky1@gmail.com>
2023-11-10 23:07:41 +0700

    save to PR pang branch

--- src/Pages/ShowPath.js
+++ src/Pages/ShowPath.js
@@ -8,7 +8,22 @@ function App() {
   return (
     <div className="App">
       <div className='bg'>
-        
+         <div className='header'>
+            <div className='leftArrow'>
+              <img src="/img/arrow.png"/>
+            </div>
+            <div className='headerText'>
+                <h2>
+                  เส้นทาง
+                </h2>
+                <h4>
+                  หอพักนิสิตจุฬา
+                </h4>
+            </div>
+            <div className='heart'>
+              <img src="/img/heart.png"/>
+            </div>
+         </div>
       </div>
       
     </div>
