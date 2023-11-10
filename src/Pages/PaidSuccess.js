5a56f3dfd3adbedbb5555b5144b6d1d4ac7b7799
Camppe <camp555555@gmail.com>
2023-11-10 15:17:44 +0700

    Camptest8

--- src/Pages/PaidSuccess.js
+++ src/Pages/PaidSuccess.js
@@ -18,12 +18,12 @@ function PaidSuccess() {
                 เลือกวิธีการชำระเงิน
             </p>
             <p >
-                        <img class="success" src="/picture/Untitled.png"></img>
-                    </p>
+                <img class="success" src="/picture/Untitled.png"></img>
+            </p>
             <p class="pay">
-                    ชำระเงินสำเร็จ
-                </p>
-           
+                ชำระเงินสำเร็จ
+            </p>
+
 
         </div>
 
