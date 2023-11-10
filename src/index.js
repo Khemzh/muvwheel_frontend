014c237c3dae33b8c2638efdcb3cd344b547cb07
Camppe <camp555555@gmail.com>
2023-11-10 15:47:25 +0700

    camp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Paid from './Pages/Paid'
+import PaidSuccess from './Pages/PaidSuccess'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Paid />
+    <PaidSuccess />
   </React.StrictMode>
 );
 
